7acf3eecb1fd1e7ccbee31330a910f6408e62447
Pete <landahoy55@gmail.com>
2018-02-19 13:43:42 +0000

    Setup DB on heroku

--- dist/config/index.js
+++ dist/config/index.js
@@ -4,8 +4,8 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.default = {
-    "port": 3005,
+    "port": process.env.PORT || 3005,
     "bodyLimit": "100kb",
-    "mongoUrl": "mongodb://localhost:27017/test"
+    "mongoUrl": process.env.MONGO_URI || "mongodb://localhost:27017/test"
 };
 //# sourceMappingURL=index.js.map
