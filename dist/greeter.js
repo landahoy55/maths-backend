e50393b0a2adc8ecdaf7dd1dec71de73bc90420f
Pete <landahoy55@gmail.com>
2018-02-19 12:41:50 +0000

    altered dis

--- dist/greeter.js
+++ dist/greeter.js
@@ -2,7 +2,7 @@
 
 // Add code to greeter.js
 function greeter() {
-    var msg = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'this stinks';
+    var msg = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'is dis working?';
 
     return msg;
 };
