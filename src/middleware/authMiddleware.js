6f98347d95eab6c04d48528cca5a9161250b67c1
Pete <landahoy55@gmail.com>
2018-04-13 12:21:25 +0100

    added more middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -31,26 +31,26 @@ let checkAdmin = (req, res, next) => {
         
         console.log("In the middleware");
 
-        // if (err) {
-        //     // return next(err);
-        //     res.status(401).json({
-        //         message: 'error'
-        //     });
-        // }
+        if (err) {
+            // return next(err);
+            res.status(401).json({
+                message: 'error'
+            });
+        }
     
-        // if (!account) {
-        //     //doesn't exist
-        //     res.status(401).json({
-        //         message: 'account not found'
-        //     });
-        // }
+        if (!account) {
+            //doesn't exist
+            res.status(401).json({
+                message: 'account not found'
+            });
+        }
         
-        // if (!user.name) {
-        //     //name property doesn't exist... change to admin
-        //     res.status(401).json({
-        //         message: 'no name property'
-        //     });
-        // }
+        if (!user.name) {
+            //name property doesn't exist... change to admin
+            res.status(401).json({
+                message: 'no name property'
+            });
+        }
     
         //hand over to passport
         next();
